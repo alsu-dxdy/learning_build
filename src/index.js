d1e5b698b0708e40c5da701f548f18a7761926dd
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-03-19 15:55:51 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+let a=0;
+
+let someStr = 'Изменил, сохранил, собрал';
+let someStr2 = 'Изменил, сохранил, собрал';
+
+let someStr3 = 'Изменил, сохранил, собрал';
+let someStr4 = 'Изменил, сохранил, собрал';
+//console.log('Hello, Webpack!');
+
+const numbers = [2, 3, 7];
+const doubledNumbers = numbers.map(number => number * 2); // Стрелочная функция. Не запнётся ли на ней Internet Explorer
+
+console.log(doubledNumbers); // 4, 6, 10
